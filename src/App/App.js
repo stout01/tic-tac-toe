7307bbcd23f7feb3aff08b6ce5fce0ab2eaa24cd
Andrew Stout <andrew@andrewstout.me>
2018-06-03 14:30:44 -0400

    Added bootstrap to project

--- src/App/App.js
+++ src/App/App.js
@@ -10,7 +10,9 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Tic-Tac-Toe</h1>
         </header>
-        <Board boardSize={3} />
+        <div className="d-flex justify-content-center mt-5">
+          <Board boardSize={3} />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
