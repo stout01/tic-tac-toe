d75d35662c085e7ce8483946fbb1064271c1dd1a
Andrew Stout <andrew@andrewstout.me>
2018-06-03 14:13:22 -0400

    Replaced default react header and content

--- src/App/App.js
+++ src/App/App.js
@@ -2,19 +2,14 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Board from './Board/Board';
-import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Tic-Tac-Toe</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Board boardSize={3} />
       </div>
     );
